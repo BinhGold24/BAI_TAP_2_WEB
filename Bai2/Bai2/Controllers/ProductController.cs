15ba995239a18663e1b26929407ea8e5d48e00d6
BinhGold24 <nb241006789@gmail.com>
2026-05-26 07:56:44 +0700

    web

---
+++ Bai2/Bai2/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Action 1: Detail
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                ViewBag.Message = "Lỗi: Bạn chưa cung cấp ID của sản phẩm trên URL!";
+            }
+            else
+            {
+                ViewBag.Message = $"Product ID = {id}";
+            }
+
+            return View(); // Trả về giao diện có chứa thanh menu
+        }
+
+        // Action 2: Category
+        public IActionResult Category(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                ViewBag.Message = "Lỗi: Bạn chưa cung cấp tên danh mục (name)!";
+            }
+            else
+            {
+                ViewBag.Message = $"Category = {name}";
+            }
+
+            return View(); // Trả về giao diện có chứa thanh menu
+        }
+    }
+}
---
+++ Bai3/Bai3/Controllers/StudentController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Info()
+        {
+            // 1. Dùng ViewBag cho Name
+            ViewBag.Name = "Nguyễn Đức Bình";
+
+            // 2. Dùng ViewData cho Age
+            ViewData["Age"] = 20;
+
+            // 3. Dùng Model cho Major
+            string major = "KHMT";
+
+            // Trả về View và truyền biến major sang dưới dạng Model
+            // Lưu ý: Phải dùng cú pháp model: major để hệ thống không nhầm chuỗi "CNTT" là tên của một file giao diện.
+            return View(model: major);
+        }
+    }
+}
